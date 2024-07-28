52d17ccfe1342d2a3a8fdcb769cdbab02fa3a1c5
RRValue <rrvalue@codeschnipsel.org>
2024-07-28 22:44:52 +0200

    enforce constexpr evaluation in binomial_coefficient_tests.cpp

--- test/binomial_coefficient_tests.cpp
+++ test/binomial_coefficient_tests.cpp
@@ -18,117 +18,182 @@ TYPED_TEST_SUITE_P(BinomialCoefficientTest);
 
 TYPED_TEST_P(BinomialCoefficientTest, FirstRow)
 {
-    EXPECT_EQ(TestFixture::TestType::get(0, 0), TestFixture::Type(1));
+    constexpr auto _0_choose_0 = TestFixture::TestType::get(0, 0);
+    constexpr auto _0_choose_1 = TestFixture::TestType::get(0, 1);
 
-    EXPECT_EQ(TestFixture::TestType::get(0, 1), TestFixture::Type(0));
+    EXPECT_EQ(_0_choose_0, TestFixture::Type(1));
+    EXPECT_EQ(_0_choose_1, TestFixture::Type(0));
 }
 
 TYPED_TEST_P(BinomialCoefficientTest, SecondRow)
 {
-    EXPECT_EQ(TestFixture::TestType::get(1, 0), TestFixture::Type(1));
-    EXPECT_EQ(TestFixture::TestType::get(1, 1), TestFixture::Type(1));
+    constexpr auto _1_choose_0 = TestFixture::TestType::get(1, 0);
+    constexpr auto _1_choose_1 = TestFixture::TestType::get(1, 1);
+    constexpr auto _1_choose_2 = TestFixture::TestType::get(1, 2);
 
-    EXPECT_EQ(TestFixture::TestType::get(1, 2), TestFixture::Type(0));
+    EXPECT_EQ(_1_choose_0, TestFixture::Type(1));
+    EXPECT_EQ(_1_choose_1, TestFixture::Type(1));
+    EXPECT_EQ(_1_choose_2, TestFixture::Type(0));
 }
 
 TYPED_TEST_P(BinomialCoefficientTest, ThirdRow)
 {
-    EXPECT_EQ(TestFixture::TestType::get(2, 0), TestFixture::Type(1));
-    EXPECT_EQ(TestFixture::TestType::get(2, 1), TestFixture::Type(2));
-    EXPECT_EQ(TestFixture::TestType::get(2, 2), TestFixture::Type(1));
+    constexpr auto _2_choose_0 = TestFixture::TestType::get(2, 0);
+    constexpr auto _2_choose_1 = TestFixture::TestType::get(2, 1);
+    constexpr auto _2_choose_2 = TestFixture::TestType::get(2, 2);
+    constexpr auto _2_choose_3 = TestFixture::TestType::get(2, 3);
 
-    EXPECT_EQ(TestFixture::TestType::get(2, 3), TestFixture::Type(0));
+    EXPECT_EQ(_2_choose_0, TestFixture::Type(1));
+    EXPECT_EQ(_2_choose_1, TestFixture::Type(2));
+    EXPECT_EQ(_2_choose_2, TestFixture::Type(1));
+    EXPECT_EQ(_2_choose_3, TestFixture::Type(0));
 }
 
 TYPED_TEST_P(BinomialCoefficientTest, FourthRow)
 {
-    EXPECT_EQ(TestFixture::TestType::get(3, 0), TestFixture::Type(1));
-    EXPECT_EQ(TestFixture::TestType::get(3, 1), TestFixture::Type(3));
-    EXPECT_EQ(TestFixture::TestType::get(3, 2), TestFixture::Type(3));
-    EXPECT_EQ(TestFixture::TestType::get(3, 3), TestFixture::Type(1));
+    constexpr auto _3_choose_0 = TestFixture::TestType::get(3, 0);
+    constexpr auto _3_choose_1 = TestFixture::TestType::get(3, 1);
+    constexpr auto _3_choose_2 = TestFixture::TestType::get(3, 2);
+    constexpr auto _3_choose_3 = TestFixture::TestType::get(3, 3);
+    constexpr auto _3_choose_4 = TestFixture::TestType::get(3, 4);
 
-    EXPECT_EQ(TestFixture::TestType::get(3, 4), TestFixture::Type(0));
+    EXPECT_EQ(_3_choose_0, TestFixture::Type(1));
+    EXPECT_EQ(_3_choose_1, TestFixture::Type(3));
+    EXPECT_EQ(_3_choose_2, TestFixture::Type(3));
+    EXPECT_EQ(_3_choose_3, TestFixture::Type(1));
+    EXPECT_EQ(_3_choose_4, TestFixture::Type(0));
 }
 
 TYPED_TEST_P(BinomialCoefficientTest, FifthRow)
 {
-    EXPECT_EQ(TestFixture::TestType::get(4, 0), TestFixture::Type(1));
-    EXPECT_EQ(TestFixture::TestType::get(4, 1), TestFixture::Type(4));
-    EXPECT_EQ(TestFixture::TestType::get(4, 2), TestFixture::Type(6));
-    EXPECT_EQ(TestFixture::TestType::get(4, 3), TestFixture::Type(4));
-    EXPECT_EQ(TestFixture::TestType::get(4, 4), TestFixture::Type(1));
+    constexpr auto _4_choose_0 = TestFixture::TestType::get(4, 0);
+    constexpr auto _4_choose_1 = TestFixture::TestType::get(4, 1);
+    constexpr auto _4_choose_2 = TestFixture::TestType::get(4, 2);
+    constexpr auto _4_choose_3 = TestFixture::TestType::get(4, 3);
+    constexpr auto _4_choose_4 = TestFixture::TestType::get(4, 4);
+    constexpr auto _4_choose_5 = TestFixture::TestType::get(4, 5);
 
-    EXPECT_EQ(TestFixture::TestType::get(4, 5), TestFixture::Type(0));
+    EXPECT_EQ(_4_choose_0, TestFixture::Type(1));
+    EXPECT_EQ(_4_choose_1, TestFixture::Type(4));
+    EXPECT_EQ(_4_choose_2, TestFixture::Type(6));
+    EXPECT_EQ(_4_choose_3, TestFixture::Type(4));
+    EXPECT_EQ(_4_choose_4, TestFixture::Type(1));
+    EXPECT_EQ(_4_choose_5, TestFixture::Type(0));
 }
 
 TYPED_TEST_P(BinomialCoefficientTest, SixthRow)
 {
-    EXPECT_EQ(TestFixture::TestType::get(5, 0), TestFixture::Type(1));
-    EXPECT_EQ(TestFixture::TestType::get(5, 1), TestFixture::Type(5));
-    EXPECT_EQ(TestFixture::TestType::get(5, 2), TestFixture::Type(10));
-    EXPECT_EQ(TestFixture::TestType::get(5, 3), TestFixture::Type(10));
-    EXPECT_EQ(TestFixture::TestType::get(5, 4), TestFixture::Type(5));
-    EXPECT_EQ(TestFixture::TestType::get(5, 5), TestFixture::Type(1));
+    constexpr auto _5_choose_0 = TestFixture::TestType::get(5, 0);
+    constexpr auto _5_choose_1 = TestFixture::TestType::get(5, 1);
+    constexpr auto _5_choose_2 = TestFixture::TestType::get(5, 2);
+    constexpr auto _5_choose_3 = TestFixture::TestType::get(5, 3);
+    constexpr auto _5_choose_4 = TestFixture::TestType::get(5, 4);
+    constexpr auto _5_choose_5 = TestFixture::TestType::get(5, 5);
+    constexpr auto _5_choose_6 = TestFixture::TestType::get(5, 6);
 
-    EXPECT_EQ(TestFixture::TestType::get(5, 6), TestFixture::Type(0));
+    EXPECT_EQ(_5_choose_0, TestFixture::Type(1));
+    EXPECT_EQ(_5_choose_1, TestFixture::Type(5));
+    EXPECT_EQ(_5_choose_2, TestFixture::Type(10));
+    EXPECT_EQ(_5_choose_3, TestFixture::Type(10));
+    EXPECT_EQ(_5_choose_4, TestFixture::Type(5));
+    EXPECT_EQ(_5_choose_5, TestFixture::Type(1));
+    EXPECT_EQ(_5_choose_6, TestFixture::Type(0));
 }
 
 TYPED_TEST_P(BinomialCoefficientTest, SeventhRow)
 {
-    EXPECT_EQ(TestFixture::TestType::get(6, 0), TestFixture::Type(1));
-    EXPECT_EQ(TestFixture::TestType::get(6, 1), TestFixture::Type(6));
-    EXPECT_EQ(TestFixture::TestType::get(6, 2), TestFixture::Type(15));
-    EXPECT_EQ(TestFixture::TestType::get(6, 3), TestFixture::Type(20));
-    EXPECT_EQ(TestFixture::TestType::get(6, 4), TestFixture::Type(15));
-    EXPECT_EQ(TestFixture::TestType::get(6, 5), TestFixture::Type(6));
-    EXPECT_EQ(TestFixture::TestType::get(6, 6), TestFixture::Type(1));
+    constexpr auto _6_choose_0 = TestFixture::TestType::get(6, 0);
+    constexpr auto _6_choose_1 = TestFixture::TestType::get(6, 1);
+    constexpr auto _6_choose_2 = TestFixture::TestType::get(6, 2);
+    constexpr auto _6_choose_3 = TestFixture::TestType::get(6, 3);
+    constexpr auto _6_choose_4 = TestFixture::TestType::get(6, 4);
+    constexpr auto _6_choose_5 = TestFixture::TestType::get(6, 5);
+    constexpr auto _6_choose_6 = TestFixture::TestType::get(6, 6);
+    constexpr auto _6_choose_7 = TestFixture::TestType::get(6, 7);
 
-    EXPECT_EQ(TestFixture::TestType::get(6, 7), TestFixture::Type(0));
+    EXPECT_EQ(_6_choose_0, TestFixture::Type(1));
+    EXPECT_EQ(_6_choose_1, TestFixture::Type(6));
+    EXPECT_EQ(_6_choose_2, TestFixture::Type(15));
+    EXPECT_EQ(_6_choose_3, TestFixture::Type(20));
+    EXPECT_EQ(_6_choose_4, TestFixture::Type(15));
+    EXPECT_EQ(_6_choose_5, TestFixture::Type(6));
+    EXPECT_EQ(_6_choose_6, TestFixture::Type(1));
+    EXPECT_EQ(_6_choose_7, TestFixture::Type(0));
 }
 
 TYPED_TEST_P(BinomialCoefficientTest, EighthRow)
 {
-    EXPECT_EQ(TestFixture::TestType::get(7, 0), TestFixture::Type(1));
-    EXPECT_EQ(TestFixture::TestType::get(7, 1), TestFixture::Type(7));
-    EXPECT_EQ(TestFixture::TestType::get(7, 2), TestFixture::Type(21));
-    EXPECT_EQ(TestFixture::TestType::get(7, 3), TestFixture::Type(35));
-    EXPECT_EQ(TestFixture::TestType::get(7, 4), TestFixture::Type(35));
-    EXPECT_EQ(TestFixture::TestType::get(7, 5), TestFixture::Type(21));
-    EXPECT_EQ(TestFixture::TestType::get(7, 6), TestFixture::Type(7));
-    EXPECT_EQ(TestFixture::TestType::get(7, 7), TestFixture::Type(1));
+    constexpr auto _7_choose_0 = TestFixture::TestType::get(7, 0);
+    constexpr auto _7_choose_1 = TestFixture::TestType::get(7, 1);
+    constexpr auto _7_choose_2 = TestFixture::TestType::get(7, 2);
+    constexpr auto _7_choose_3 = TestFixture::TestType::get(7, 3);
+    constexpr auto _7_choose_4 = TestFixture::TestType::get(7, 4);
+    constexpr auto _7_choose_5 = TestFixture::TestType::get(7, 5);
+    constexpr auto _7_choose_6 = TestFixture::TestType::get(7, 6);
+    constexpr auto _7_choose_7 = TestFixture::TestType::get(7, 7);
+    constexpr auto _7_choose_8 = TestFixture::TestType::get(7, 8);
 
-    EXPECT_EQ(TestFixture::TestType::get(7, 8), TestFixture::Type(0));
+    EXPECT_EQ(_7_choose_0, TestFixture::Type(1));
+    EXPECT_EQ(_7_choose_1, TestFixture::Type(7));
+    EXPECT_EQ(_7_choose_2, TestFixture::Type(21));
+    EXPECT_EQ(_7_choose_3, TestFixture::Type(35));
+    EXPECT_EQ(_7_choose_4, TestFixture::Type(35));
+    EXPECT_EQ(_7_choose_5, TestFixture::Type(21));
+    EXPECT_EQ(_7_choose_6, TestFixture::Type(7));
+    EXPECT_EQ(_7_choose_7, TestFixture::Type(1));
+    EXPECT_EQ(_7_choose_8, TestFixture::Type(0));
 }
 
 TYPED_TEST_P(BinomialCoefficientTest, NinethRow)
 {
-    EXPECT_EQ(TestFixture::TestType::get(8, 0), TestFixture::Type(1));
-    EXPECT_EQ(TestFixture::TestType::get(8, 1), TestFixture::Type(8));
-    EXPECT_EQ(TestFixture::TestType::get(8, 2), TestFixture::Type(28));
-    EXPECT_EQ(TestFixture::TestType::get(8, 3), TestFixture::Type(56));
-    EXPECT_EQ(TestFixture::TestType::get(8, 4), TestFixture::Type(70));
-    EXPECT_EQ(TestFixture::TestType::get(8, 5), TestFixture::Type(56));
-    EXPECT_EQ(TestFixture::TestType::get(8, 6), TestFixture::Type(28));
-    EXPECT_EQ(TestFixture::TestType::get(8, 7), TestFixture::Type(8));
-    EXPECT_EQ(TestFixture::TestType::get(8, 8), TestFixture::Type(1));
+    constexpr auto _8_choose_0 = TestFixture::TestType::get(8, 0);
+    constexpr auto _8_choose_1 = TestFixture::TestType::get(8, 1);
+    constexpr auto _8_choose_2 = TestFixture::TestType::get(8, 2);
+    constexpr auto _8_choose_3 = TestFixture::TestType::get(8, 3);
+    constexpr auto _8_choose_4 = TestFixture::TestType::get(8, 4);
+    constexpr auto _8_choose_5 = TestFixture::TestType::get(8, 5);
+    constexpr auto _8_choose_6 = TestFixture::TestType::get(8, 6);
+    constexpr auto _8_choose_7 = TestFixture::TestType::get(8, 7);
+    constexpr auto _8_choose_8 = TestFixture::TestType::get(8, 8);
+    constexpr auto _8_choose_9 = TestFixture::TestType::get(8, 9);
 
-    EXPECT_EQ(TestFixture::TestType::get(8, 9), TestFixture::Type(0));
+    EXPECT_EQ(_8_choose_0, TestFixture::Type(1));
+    EXPECT_EQ(_8_choose_1, TestFixture::Type(8));
+    EXPECT_EQ(_8_choose_2, TestFixture::Type(28));
+    EXPECT_EQ(_8_choose_3, TestFixture::Type(56));
+    EXPECT_EQ(_8_choose_4, TestFixture::Type(70));
+    EXPECT_EQ(_8_choose_5, TestFixture::Type(56));
+    EXPECT_EQ(_8_choose_6, TestFixture::Type(28));
+    EXPECT_EQ(_8_choose_7, TestFixture::Type(8));
+    EXPECT_EQ(_8_choose_8, TestFixture::Type(1));
+    EXPECT_EQ(_8_choose_9, TestFixture::Type(0));
 }
 
 TYPED_TEST_P(BinomialCoefficientTest, TenthRow)
 {
-    EXPECT_EQ(TestFixture::TestType::get(9, 0), TestFixture::Type(1));
-    EXPECT_EQ(TestFixture::TestType::get(9, 1), TestFixture::Type(9));
-    EXPECT_EQ(TestFixture::TestType::get(9, 2), TestFixture::Type(36));
-    EXPECT_EQ(TestFixture::TestType::get(9, 3), TestFixture::Type(84));
-    EXPECT_EQ(TestFixture::TestType::get(9, 4), TestFixture::Type(126));
-    EXPECT_EQ(TestFixture::TestType::get(9, 5), TestFixture::Type(126));
-    EXPECT_EQ(TestFixture::TestType::get(9, 6), TestFixture::Type(84));
-    EXPECT_EQ(TestFixture::TestType::get(9, 7), TestFixture::Type(36));
-    EXPECT_EQ(TestFixture::TestType::get(9, 8), TestFixture::Type(9));
-    EXPECT_EQ(TestFixture::TestType::get(9, 9), TestFixture::Type(1));
+    constexpr auto _9_choose_0 = TestFixture::TestType::get(9, 0);
+    constexpr auto _9_choose_1 = TestFixture::TestType::get(9, 1);
+    constexpr auto _9_choose_2 = TestFixture::TestType::get(9, 2);
+    constexpr auto _9_choose_3 = TestFixture::TestType::get(9, 3);
+    constexpr auto _9_choose_4 = TestFixture::TestType::get(9, 4);
+    constexpr auto _9_choose_5 = TestFixture::TestType::get(9, 5);
+    constexpr auto _9_choose_6 = TestFixture::TestType::get(9, 6);
+    constexpr auto _9_choose_7 = TestFixture::TestType::get(9, 7);
+    constexpr auto _9_choose_8 = TestFixture::TestType::get(9, 8);
+    constexpr auto _9_choose_9 = TestFixture::TestType::get(9, 9);
+    constexpr auto _9_choose_10 = TestFixture::TestType::get(9, 10);
 
-    EXPECT_EQ(TestFixture::TestType::get(9, 10), TestFixture::Type(0));
+    EXPECT_EQ(_9_choose_0, TestFixture::Type(1));
+    EXPECT_EQ(_9_choose_1, TestFixture::Type(9));
+    EXPECT_EQ(_9_choose_2, TestFixture::Type(36));
+    EXPECT_EQ(_9_choose_3, TestFixture::Type(84));
+    EXPECT_EQ(_9_choose_4, TestFixture::Type(126));
+    EXPECT_EQ(_9_choose_5, TestFixture::Type(126));
+    EXPECT_EQ(_9_choose_6, TestFixture::Type(84));
+    EXPECT_EQ(_9_choose_7, TestFixture::Type(36));
+    EXPECT_EQ(_9_choose_8, TestFixture::Type(9));
+    EXPECT_EQ(_9_choose_9, TestFixture::Type(1));
+    EXPECT_EQ(_9_choose_10, TestFixture::Type(0));
 }
 
 REGISTER_TYPED_TEST_SUITE_P(BinomialCoefficientTest,  //
