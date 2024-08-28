d4401179b407545d8c6fc4bf66285a67c4f08589
RRValue <rrvalue@codeschnipsel.org>
2024-08-28 23:53:01 +0200

    impl generation of split matrix + tests

--- src/NBezier/split_matrices.h
+++ src/NBezier/split_matrices.h
@@ -3,6 +3,7 @@
 #include "NBezier/defines.h"
 
 #include "NBezier/bezier_requirements.h"
+#include "NBezier/split_weight_matrix.h"
 
 #include <boost/qvm/mat.hpp>
 
@@ -27,9 +28,89 @@ struct SplitMatrices
 
     template<typename Scalar, size_t Degree>
         requires SplitMatricesRequirement<Scalar, Degree>
-    static constexpr auto get()
+    static constexpr auto get(const Scalar& alpha) noexcept
     {
-        return SplitMatriesResult<Scalar, Degree>{};
+        auto result = SplitMatriesResult<Scalar, Degree>{SplitWeightMatrix::getLeft<Scalar, Degree>(),  //
+                                                         SplitWeightMatrix::getRight<Scalar, Degree>()};
+
+        generate<Scalar, Degree>(result, alpha);
+
+        return result;
+    }
+
+private:
+    template<typename Scalar>
+    struct UV
+    {
+        Scalar u;
+        Scalar v;
+
+        Scalar u_accum;
+        Scalar v_accum;
+    };
+
+private:
+    template<typename Scalar, size_t Degree>
+    NBInline static constexpr auto generate(auto& matrices, const Scalar& alpha)
+    {
+        auto params = UV{alpha, Scalar(1) - alpha, Scalar(1), Scalar(1)};
+
+        assignPotences<Degree>(matrices, params, std::make_index_sequence<Degree - 1>{});
+
+        return matrices;
+    }
+
+    template<size_t Degree, size_t... Steps>
+    NBInline static constexpr void assignPotences(auto& matrices, auto& params, std::index_sequence<Steps...>)
+    {
+        (assignPotence<Steps, Degree>(matrices, params), ...);
+    }
+
+    template<size_t Step, size_t Degree>
+    NBInline static constexpr void assignPotence(auto& matrices, auto& params)
+    {
+        params.u_accum *= params.u;
+        params.v_accum *= params.v;
+
+        constexpr auto assign_steps = std::make_index_sequence<Degree - Step - 1>{};
+
+        assignRow<Step + 1>(matrices.m_left, params.u_accum, assign_steps);
+        assignRowInverse<Degree, Step + 1>(matrices.m_right, params.v_accum, assign_steps);
+        assignDiagonal<0, Step + 1>(matrices.m_left, params.v_accum, assign_steps);
+        assignDiagonalInverse<Degree, 0, Step + 1>(matrices.m_right, params.u_accum, assign_steps);
+    }
+
+    template<size_t Row, size_t... Steps>
+    NBInline static constexpr void assignRow(auto& dst, const auto& value, std::index_sequence<Steps...>)
+    {
+        (assignCell<Row, Row + Steps>(dst, value), ...);
+    }
+
+    template<size_t Degree, size_t Row, size_t... Steps>
+    NBInline static constexpr void assignRowInverse(auto& dst, const auto& value, std::index_sequence<Steps...>)
+    {
+        (assignCell<Degree - 1 - Row, Degree - 1 - (Row + Steps)>(dst, value), ...);
+    }
+
+    template<size_t Row, size_t Column, size_t... Steps>
+    NBInline static constexpr void assignDiagonal(auto& dst, const auto& value, std::index_sequence<Steps...>)
+    {
+        (assignCell<Row + Steps, Column + Steps>(dst, value), ...);
+    }
+
+    template<size_t Degree, size_t Row, size_t Column, size_t... Steps>
+    NBInline static constexpr void assignDiagonalInverse(auto& dst, const auto& value, std::index_sequence<Steps...>)
+    {
+        (assignCell<Degree - 1 - (Row + Steps), Degree - 1 - (Column + Steps)>(dst, value), ...);
+    }
+
+    template<size_t Row, size_t Column>
+    NBInline static constexpr void assignCell(auto& dst, const auto& value)
+    {
+        constexpr auto row = Row;
+        constexpr auto column = Column;
+
+        boost::qvm::A<row, column>(dst) *= value;
     }
 };
 
--- test/split_matrices_tests.cpp
+++ test/split_matrices_tests.cpp
@@ -25,48 +25,78 @@ TYPED_TEST_P(SplitMatricesGenerationTest, Get)
     using S = TestFixture::Scalar;
 
     {
-        constexpr auto m = SplitMatrices::get<S, 1>();
-        constexpr auto l = boost::qvm::mat<S, 1, 1>{};
-        constexpr auto r = boost::qvm::mat<S, 1, 1>{};
+        // clang-format off
+        constexpr auto m = SplitMatrices::get<S, 1>(S(3));
+        constexpr auto l = boost::qvm::mat<S, 1, 1>{1};
+        constexpr auto r = boost::qvm::mat<S, 1, 1>{1};
+        // clang-format on
 
         EXPECT_EQ(m.m_left, l);
-        EXPECT_EQ(m.m_left, r);
+        EXPECT_EQ(m.m_right, r);
     }
 
     {
-        constexpr auto m = SplitMatrices::get<S, 2>();
-        constexpr auto l = boost::qvm::mat<S, 2, 2>{};
-        constexpr auto r = boost::qvm::mat<S, 2, 2>{};
+        // clang-format off
+        constexpr auto m = SplitMatrices::get<S, 2>(S(3));
+        constexpr auto l = boost::qvm::mat<S, 2, 2>{1, 1 * -2, //
+                                                    0, 1 *  3};
+        constexpr auto r = boost::qvm::mat<S, 2, 2>{1 * -2, 0,  //
+                                                    1 *  3, 1};
+        // clang-format on
 
         EXPECT_EQ(m.m_left, l);
-        EXPECT_EQ(m.m_left, r);
+        EXPECT_EQ(m.m_right, r);
     }
 
     {
-        constexpr auto m = SplitMatrices::get<S, 3>();
-        constexpr auto l = boost::qvm::mat<S, 3, 3>{};
-        constexpr auto r = boost::qvm::mat<S, 3, 3>{};
+        // clang-format off
+        constexpr auto m = SplitMatrices::get<S, 3>(S(3));
+        constexpr auto l = boost::qvm::mat<S, 3, 3>{1, 1 * -2, 1 * -2 * -2, //
+                                                    0, 1 *  3, 2 * -2 *  3, //
+                                                    0, 0     , 1 *  3 *  3};
+        constexpr auto r = boost::qvm::mat<S, 3, 3>{1 * -2 * -2, 0     , 0,  //
+                                                    2 * -2 * 3 , 1 * -2, 0,  //
+                                                    1 *  3 * 3 , 1 *  3, 1};
+        // clang-format on
 
         EXPECT_EQ(m.m_left, l);
-        EXPECT_EQ(m.m_left, r);
+        EXPECT_EQ(m.m_right, r);
     }
 
     {
-        constexpr auto m = SplitMatrices::get<S, 4>();
-        constexpr auto l = boost::qvm::mat<S, 4, 4>{};
-        constexpr auto r = boost::qvm::mat<S, 4, 4>{};
+        // clang-format off
+        constexpr auto m = SplitMatrices::get<S, 4>(S(3));
+        constexpr auto l = boost::qvm::mat<S, 4, 4>{1, 1 * -2, 1 * -2 * -2, 1 * -2 * -2 * -2, //
+                                                    0, 1 *  3, 2 * -2 *  3, 3 * -2 * -2 *  3, //
+                                                    0, 0     , 1 *  3 *  3, 3 * -2 *  3 *  3, //
+                                                    0, 0     , 0          , 1 *  3 *  3 *  3};
+        constexpr auto r = boost::qvm::mat<S, 4, 4>{1 * -2 * -2 * -2, 0          , 0     , 0,  //
+                                                    3 * -2 * -2 *  3, 1 * -2 * -2, 0     , 0,  //
+                                                    3 * -2 *  3 *  3, 2 * -2 * 3 , 1 * -2, 0,  //
+                                                    1 *  3 *  3 *  3, 1 *  3 * 3 , 1 *  3, 1};
+        // clang-format on
 
         EXPECT_EQ(m.m_left, l);
-        EXPECT_EQ(m.m_left, r);
+        EXPECT_EQ(m.m_right, r);
     }
 
     {
-        constexpr auto m = SplitMatrices::get<S, 5>();
-        constexpr auto l = boost::qvm::mat<S, 5, 5>{};
-        constexpr auto r = boost::qvm::mat<S, 5, 5>{};
+        // clang-format off
+        constexpr auto m = SplitMatrices::get<S, 5>(S(3));
+        constexpr auto l = boost::qvm::mat<S, 5, 5>{1, 1 * -2, 1 * -2 * -2, 1 * -2 * -2 * -2, 1 * -2 * -2 * -2 * -2, //
+                                                    0, 1 *  3, 2 * -2 *  3, 3 * -2 * -2 *  3, 4 * -2 * -2 * -2 *  3, //
+                                                    0, 0     , 1 *  3 *  3, 3 * -2 *  3 *  3, 6 * -2 * -2 *  3 *  3, //
+                                                    0, 0     , 0          , 1 *  3 *  3 *  3, 4 * -2 *  3 *  3 *  3, //
+                                                    0, 0     , 0          , 0               , 1 *  3 *  3 *  3 *  3};
+        constexpr auto r = boost::qvm::mat<S, 5, 5>{1 * -2 * -2 * -2 * -2, 0               , 0          , 0     , 0,  //
+                                                    4 * -2 * -2 * -2 *  3, 1 * -2 * -2 * -2, 0          , 0     , 0,  //
+                                                    6 * -2 * -2 *  3 *  3, 3 * -2 * -2 *  3, 1 * -2 * -2, 0     , 0,  //
+                                                    4 * -2 *  3 *  3 *  3, 3 * -2 *  3 *  3, 2 * -2 * 3 , 1 * -2, 0,  //
+                                                    1 *  3 *  3 *  3 *  3, 1 *  3 *  3 *  3, 1 *  3 * 3 , 1 *  3, 1};
+        // clang-format on
 
         EXPECT_EQ(m.m_left, l);
-        EXPECT_EQ(m.m_left, r);
+        EXPECT_EQ(m.m_right, r);
     }
 }
 
