45f5aa91f010d303e0ce6969272106ec1544f29d
RRValue <rrvalue@codeschnipsel.org>
2024-09-09 18:28:14 +0200

    at length benchmark

---
+++ benchmark/bezier_length_benchmark.cpp
@@ -0,0 +1,37 @@
+#include "NBezier/bezier.h"
+#include "NBezier/bezier_length.h"
+
+#include <benchmark/benchmark.h>
+
+UseNameSpace(NBezier);
+
+static void BM_BezierLengthCompileTime(benchmark::State& state)
+{
+    constexpr auto p0 = Point<float, 2>{-1, 1};
+    constexpr auto p1 = Point<float, 2>{0, -1};
+    constexpr auto p2 = Point<float, 2>{1, 1};
+
+    constexpr auto bezier = BezierPoints<float, 2, 2>{p0, p1, p2};
+
+    for(auto _ : state)
+    {
+        [[maybe_unused]] constexpr auto length_compile_time = BezierLength::get(bezier);
+    }
+}
+
+static void BM_BezierLengthRunTime(benchmark::State& state)
+{
+    constexpr auto p0 = Point<float, 2>{-1, 1};
+    constexpr auto p1 = Point<float, 2>{0, -1};
+    constexpr auto p2 = Point<float, 2>{1, 1};
+
+    constexpr auto bezier = BezierPoints<float, 2, 2>{p0, p1, p2};
+
+    for(auto _ : state)
+    {
+        benchmark::DoNotOptimize(BezierLength::get(bezier));
+    }
+}
+
+BENCHMARK(BM_BezierLengthCompileTime);
+BENCHMARK(BM_BezierLengthRunTime);
