594fd6a981d88634b5bc14e020aab2232910e026
RRValue <rrvalue@codeschnipsel.org>
2024-09-09 18:14:55 +0200

    remove maybe_unused

--- test/bezier_length_tests.cpp
+++ test/bezier_length_tests.cpp
@@ -13,10 +13,10 @@ TEST(Bezier, Length)
     constexpr auto p1 = Point<float, 2>{0, -1};
     constexpr auto p2 = Point<float, 2>{1, 1};
 
-    [[maybe_unused]] constexpr auto bezier = BezierPoints<float, 2, 2>{p0, p1, p2};
-    [[maybe_unused]] constexpr auto exp_length = float(2.957885715);
-    [[maybe_unused]] constexpr auto length_compile_time = BezierLength::get(bezier);
-    [[maybe_unused]] const auto length_run_time = BezierLength::get(bezier);
+    constexpr auto bezier = BezierPoints<float, 2, 2>{p0, p1, p2};
+    constexpr auto exp_length = float(2.957885715);
+    constexpr auto length_compile_time = BezierLength::get(bezier);
+    const auto length_run_time = BezierLength::get(bezier);
 
     EXPECT_EQ(length_compile_time, length_run_time);
     EXPECT_TRUE(std::abs(exp_length - length_compile_time) < float(1e-05));
