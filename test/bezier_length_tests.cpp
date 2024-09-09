876016ce486bfaa38a8b74c092c8381fab96c332
RRValue <rrvalue@codeschnipsel.org>
2024-09-09 18:14:02 +0200

    put values for constexpr limits down to reasonable values, test if compile time and run time length is equal

--- test/bezier_length_tests.cpp
+++ test/bezier_length_tests.cpp
@@ -15,7 +15,10 @@ TEST(Bezier, Length)
 
     [[maybe_unused]] constexpr auto bezier = BezierPoints<float, 2, 2>{p0, p1, p2};
     [[maybe_unused]] constexpr auto exp_length = float(2.957885715);
-    [[maybe_unused]] constexpr auto length = BezierLength::get(bezier);
+    [[maybe_unused]] constexpr auto length_compile_time = BezierLength::get(bezier);
+    [[maybe_unused]] const auto length_run_time = BezierLength::get(bezier);
 
-    EXPECT_TRUE(std::abs(exp_length - length) < float(1e-05));
+    EXPECT_EQ(length_compile_time, length_run_time);
+    EXPECT_TRUE(std::abs(exp_length - length_compile_time) < float(1e-05));
+    EXPECT_TRUE(std::abs(exp_length - length_run_time) < float(1e-05));
 }
